006e2cda2017d54cab069662d6f8ec9802d0cc94
Ratana10 <sanratana18@gmail.com>
2026-01-20 23:17:00 +0700

    20260120: create model and migration

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const db = require("./models")
 const app = express()
 const port = 3000
+
+db.sequelize
+.authenticate()
+.then(()=> console.log("Database connected successfully"))
+.catch((err) => console.log("Unable connect to database", err))
+
+
 const products = [
   {
     id: 1,
@@ -23,6 +31,34 @@ app.post("/products", (req, res) => {
   // Business logic
 
   res.send("Product created successfully")
+})
+
+const { Category } = require('./models')
+
+app.use(express.json())
+
+app.post("/api/v1/categories", async (req, res) => {
+  // Business logic
+
+  const name = req.body.name
+  const isActive = req.body.isActive
+
+ const created =  await Category.create({ name, isActive })
+
+  res.json({
+    message: "Category created successfully",
+    data: created
+  })
+})
+
+app.get("/api/v1/categories", async (req, res) => {
+
+ const categories =  await Category.findAll()
+
+  res.json({
+    message: "Category fetched successfully",
+    data: categories
+  })
 })
 
 
