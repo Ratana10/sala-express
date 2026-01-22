4acc74dbc16d71f88a0129b5795b5531f5b8fab6
Ratana10 <sanratana18@gmail.com>
2026-01-22 23:13:26 +0700

    20260122: create model relationship

--- index.js
+++ index.js
@@ -1,67 +1,87 @@
-const express = require('express')
-const db = require("./models")
-const app = express()
-const port = 3000
+const express = require("express");
+const db = require("./models");
+const app = express();
+const port = 3000;
+const { Category, Product } = require("./models");
+
+app.use(express.json());
 
 db.sequelize
-.authenticate()
-.then(()=> console.log("Database connected successfully"))
-.catch((err) => console.log("Unable connect to database", err))
+  .authenticate()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.log("Unable connect to database", err));
 
-
-const products = [
-  {
-    id: 1,
-    name: "Apple",
-    price: 20,
-    qty: 10,
+app.get("/api/v1/products", async (req, res) => {
+  try {
+    const products = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          as: "category"
+        }
+      ]
+    });
+    res.json({
+      message: "Product fetched successfully",
+      data: products,
+    });
+  } catch (error) {
+    console.log("Creating product error:", error);
   }
-]
-app.get('/', (req, res) => {
-  res.send(products)
-})
+});
 
-app.get("/products", (req, res) => {
-  // Business logic
+app.post("/api/v1/products", async (req, res) => {
+  // const name = req.body.name
+  // const price = req.body.price
+  // const categroyId = req.body.categroyId
+  try {
+    const { name, price, categoryId, isActive } = req.body;
 
-  res.send(products)
-})
-
-app.post("/products", (req, res) => {
-  // Business logic
-
-  res.send("Product created successfully")
-})
-
-const { Category } = require('./models')
-
-app.use(express.json())
+    const createdProduct = await Product.create({
+      name,
+      price,
+      categoryId,
+      isActive,
+    });
+    res.json({
+      message: "Product created successfully",
+      data: createdProduct,
+    });
+  } catch (error) {
+    console.log("Creating product error:", error);
+  }
+});
 
 app.post("/api/v1/categories", async (req, res) => {
   // Business logic
 
-  const name = req.body.name
-  const isActive = req.body.isActive
+  const name = req.body.name;
+  const isActive = req.body.isActive;
 
- const created =  await Category.create({ name, isActive })
+  const created = await Category.create({ name, isActive });
 
   res.json({
     message: "Category created successfully",
-    data: created
-  })
-})
+    data: created,
+  });
+});
 
 app.get("/api/v1/categories", async (req, res) => {
-
- const categories =  await Category.findAll()
+  const categories = await Category.findAll({
+    include: [
+      {
+        model: Product,
+        as: "products"
+      },
+    ]
+  });
 
   res.json({
     message: "Category fetched successfully",
-    data: categories
-  })
-})
-
+    data: categories,
+  });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ models/product.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Product extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      console.log("Models", models)
+      // define association here
+      Product.belongsTo(models.Category, {
+        foreignKey: "categoryId",
+        as: "category",
+      });
+    }
+  }
+  Product.init(
+    {
+      name: DataTypes.STRING,
+      categoryId: DataTypes.INTEGER,
+      price: DataTypes.DECIMAL,
+      qty: DataTypes.INTEGER,
+      isActive: DataTypes.BOOLEAN,
+    },
+    {
+      sequelize,
+      modelName: "Product",
+    },
+  );
+  return Product;
+};
